6687a71d68f2c7668b769e1f96bfc5dec9f7b215
tariqshaban <tariqshaban1999@yahoo.com>
2021-12-12 14:11:58 +0200

    Modified main.py

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
-import datetime
-
-import pandas as pd
-
 from providers.plots_provider import PlotsProvider
-from providers.forum_scraper import ForumScraper
 
 print('Scraping....')
 print('--------------------------------------------------')
-# pd.set_option('display.max_rows', None)
-# pd.set_option('display.max_columns', None)
 
-# df = ForumScraper.scrap_threads(fast_fetch=True)
-#
-# print(df)
-# df = ForumScraper.scrap_threads()
-# ForumScraper.cache_threads()
-# ForumScraper.cache_threads_details()
-# print(ForumScraper.scrap_threads_details(fast_fetch=True))
+# Fetches from threads
+PlotsProvider.plot_threads_posting()
+PlotsProvider.plot_views_with_replies()
+PlotsProvider.plot_view_with_replies_relation()
+PlotsProvider.plot_top_15_thread_creators()
+PlotsProvider.plot_top_15_oldest_threads()
+PlotsProvider.plot_locked_sticky_threads()
 
+# Fetches from thread's details
+PlotsProvider.plot_replies()
+PlotsProvider.plot_top_15_repliers()
+PlotsProvider.plot_top_15_messages()
+PlotsProvider.plot_user_titles()
+PlotsProvider.plot_user_banners()
+PlotsProvider.plot_users_joining()
+PlotsProvider.plot_user_top_10_locations()
 
-# PlotsProvider.plot_threads_posting(fast_fetch=True)
-# PlotsProvider.plot_views_with_replies(fast_fetch=True)
-# PlotsProvider.plot_monthly_views_with_replies(fast_fetch=True)
-# PlotsProvider.plot_daily_views_with_replies(fast_fetch=True)
-# PlotsProvider.plot_view_with_replies_relation(fast_fetch=True)
-# PlotsProvider.plot_top_15_thread_creators(fast_fetch=True)
-# PlotsProvider.plot_locked_sticky_threads(fast_fetch=True)
-PlotsProvider.plot_top_15_oldest_threads(fast_fetch=True)
-#
-# PlotsProvider.plot_top_15_repliers(fast_fetch=True, fast_fetch_threads=True)
-# PlotsProvider.plot_top_15_messages(fast_fetch=True, fast_fetch_threads=True)
-# PlotsProvider.plot_user_titles(fast_fetch=True, fast_fetch_threads=True)
-# PlotsProvider.plot_user_banners(fast_fetch=True, fast_fetch_threads=True)
-# PlotsProvider.plot_users_joining(fast_fetch=True, fast_fetch_threads=True)
-# PlotsProvider.plot_user_top_10_locations(fast_fetch=True, fast_fetch_threads=True)
 print('--------------------------------------------------')
 print('Done.')
